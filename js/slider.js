35799090aec23c42d3cd0aa24c42f4e717f7cc28
callmervdio <nikitkaaaaa@gmail.com>
2021-03-20 00:01:34 +0300

    Выполнил правки

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+let initSlider = function() {
+  let sliderBlock = document.querySelector('.about');
+  let sliderToggles = sliderBlock.querySelectorAll('.slider-controls button');
+  let sliderItems = sliderBlock.querySelectorAll('.about-item');
+  let currentSlide = 0;
+
+  sliderToggles.forEach(function(sliderToggle, index) {
+    sliderToggle.addEventListener('click', function() {
+      sliderItems[currentSlide].classList.remove('about-item_current');
+      sliderToggles[currentSlide].classList.remove('slider-control_current');
+
+      sliderItems[index].classList.add('about-item_current');
+      sliderToggle.classList.add('slider-control_current');
+
+      currentSlide = index;
+    })
+  })
+};
+
+initSlider();
